4558b41f9120249b8d67c33fd7f48d91b197b6ab
Michael Struwig <turnthescrewdriver@gmail.com>
2018-10-29 10:43:34 +0200

    Add LabeledProcessor and Evaluator object

---
+++ tests/mechanical_system/test_evaluator.py
@@ -0,0 +1,32 @@
+import unittest
+import numpy as np
+import pandas as pd
+
+from numpy.testing import assert_array_equal
+
+# Local imports
+from unified_model.mechanical_system.evaluator import LabeledProcessor, Evaluator
+
+
+class TestLabeledProcessor(unittest.TestCase):
+    """Test the `LabeledProcessor` class."""
+
+    def setUp(self):
+        """Run before every test"""
+
+        self.test_groundtruth_df = pd.DataFrame({'start_x': [10, 11],
+                                                 'start_y': [10, 20],
+                                                 'end_x': [10, 11],
+                                                 'end_y': [20, 30],
+                                                 'top_of_magnet': [0, 1],
+                                                 'y_pixel_scale': [0.2, 0.2]})
+
+    def test_fit_transform(self):
+        """Test the `fit_transform method`."""
+        test_lp = LabeledProcessor(L=120, mf=10, mm=10)
+        actual_result = test_lp.fit_transform(self.test_groundtruth_df)
+        expected_result = np.array([108., 98.])
+
+        assert_array_equal(actual_result, expected_result)
+
+
