1cbe94a6b6c33bea08f20eb429f59cca61179cfb
Michael Struwig <turnthescrewdriveer@gmail.com>
2019-02-09 15:01:54 +0200

    Add additional testing / debugging tools for emulating e-system.

---
+++ unified_model/utils/testing/__init__.py
@@ -0,0 +1,5 @@
+"""
+This file holds functions and class that are useful for doing ad-hoc simulation
+and testing runs while either debugging or experimenting with the various
+moving parts of the `unified_model` project.
+"""
