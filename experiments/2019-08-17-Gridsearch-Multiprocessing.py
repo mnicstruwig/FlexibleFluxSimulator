1b6d5d13509a1cff1ab5f931adf036f74f07660c
Michael Struwig <michael@praelexis.com>
2019-08-31 11:57:29 +0200

    Update .gitignore + Turn things into functions

--- experiments/2019-08-17-Gridsearch-Multiprocessing.py
+++ experiments/2019-08-17-Gridsearch-Multiprocessing.py
@@ -18,45 +18,35 @@ from unified_model.metrics import (dtw_euclid_distance,
                                    root_mean_square_percentage_diff)
 from unified_model.unified import UnifiedModel
 from unified_model.utils.utils import (build_paramater_grid, collect_samples,
-                                       update_nested_attributes)
+                                       update_nested_attributes, interpolate_and_resample)
 
 warnings.filterwarnings('ignore', category=FutureWarning)
 
-base_unified_model = UnifiedModel.load_from_disk('../my_saved_model/')
 
-base_groundtruth_path = './data/2019-05-23/'
+def make_groundtruth_dict(target_time, target_values, label):
+    return {
+        'time': target_time,
+        'values': target_values,
+        'label': label
+    }
 
-pixel_scales = {'A': 0.18451, 'B': 0.148148}
-seconds_per_frame = {'A': 1/80, 'B': 1/60}
-samples = {}
-samples['A'] = collect_samples(base_path=base_groundtruth_path,
-                               acc_pattern='A/*acc*.csv',
-                               adc_pattern='A/*adc*.csv',
-                               labeled_video_pattern='A/*labels*.csv')
-samples['B'] = collect_samples(base_path=base_groundtruth_path,
-                               acc_pattern='B/*acc*.csv',
-                               adc_pattern='B/*adc*.csv',
-                               labeled_video_pattern='B/*labels*.csv')
-accelerometer_inputs = {}
-accelerometer_inputs['A'] = [
-    AccelerometerInput(raw_accelerometer_input=sample.acc_df,
-                       accel_column='z_G',
-                       time_column='time(ms)',
-                       accel_unit='g',
-                       time_unit='ms',
-                       smooth=True,
-                       interpolate=True) for sample in samples['A']
-]
 
-accelerometer_inputs['B'] = [
-    AccelerometerInput(raw_accelerometer_input=sample.acc_df,
-                       accel_column='z_G',
-                       time_column='time(ms)',
-                       accel_unit='g',
-                       time_unit='ms',
-                       smooth=True,
-                       interpolate=True) for sample in samples['B']
-]
+def export_groundtruth(which_device, which_sample, *args):
+    """Example element of *args:
+
+    a_groundtruth_dict = {
+        'time': y_time_target,
+        'value': y_target,
+        'signal': 'y'
+    }
+
+    Exported in "tidy" format.
+
+    """
+    df = pd.concat([pd.DataFrame(dict_) for dict_ in args])
+    df.to_parquet(f'{which_device}_{which_sample}_groundtruth.parq',
+                  engine='pyarrow',
+                  compression='brotli')
 
 
 def make_mechanical_spring(damper_constant):
@@ -67,103 +57,57 @@ def make_mechanical_spring(damper_constant):
                             damper_constant=damper_constant)
 
 
-####################
-which_device = 'A'
-which_sample = 2
+def get_mechanical_groundtruth(which_device, which_sample):
+    labeled_video_processor = LabeledVideoProcessor(
+        L=125,
+        mm=10,
+        seconds_per_frame=seconds_per_frame,
+        pixel_scale=pixel_scale)
 
-pixel_scale = pixel_scales[which_device]
-seconds_per_frame = seconds_per_frame[which_device]
+    y_target, y_time_target = labeled_video_processor.fit_transform(
+        samples[which_device][which_sample].video_labels_df,
+        impute_missing_values=True
+    )
+    # SMOOTH TARGET VALUES  (OPTIONAL?)
+    y_target = signal.savgol_filter(y_target, 9, 3)
 
-accelerometer_inputs = accelerometer_inputs[which_device]
-input_ = [which_sample]
-####################
+    return make_groundtruth_dict(y_time_target, y_target, 'y')
 
 
-# Gridsearch parameters
-damping_coefficients = np.linspace(0.02, 0.05, 20)
-mech_spring_coefficients = np.linspace(0, 0.5, 5) #[0.0125]  # Found from investigation
-constant_coupling_values = np.linspace(0.1, 3, 20)
-rectification_drop = [0.10]
-flux_models = [which_device]
-dflux_models = [which_device]
-coil_resistance = [which_device]
+def get_electrical_groundtruth(which_device, which_sample):
+    voltage_division_ratio = 1/0.342
+    adc_processor = AdcProcessor(
+        voltage_division_ratio=voltage_division_ratio,
+        smooth=True)
 
-# Overrides (set parameters for test)
-# damping_coefficients = [0.035]
-# mech_spring_coefficients = [0.0000]
-# constant_coupling_values = [0.5]
+    emf_target, emf_time_target = adc_processor.fit_transform(
+        samples[which_device][which_sample].adc_df
+    )
+    return make_groundtruth_dict(emf_time_target, emf_target, 'emf')
 
-param_dict = {
-    'mechanical_model.input_': input_,
-    'mechanical_model.damper': damping_coefficients,
-    'mechanical_model.mechanical_spring': mech_spring_coefficients,
-    'coupling_model': constant_coupling_values,
-    'electrical_model.rectification_drop': rectification_drop,
-    'electrical_model.flux_model': flux_models,
-    'electrical_model.dflux_model': dflux_models,
-    'electrical_model.coil_resistance': coil_resistance
-}
 
-func_dict = {
-    'mechanical_model.input_': lambda x: accelerometer_inputs[x],
-    'mechanical_model.damper': DamperConstant,
-    'mechanical_model.mechanical_spring': make_mechanical_spring,
-    'coupling_model': ConstantCoupling,
-    'electrical_model.rectification_drop': lambda x: x,
-    'electrical_model.flux_model': lambda x: abc.flux_models[x],
-    'electrical_model.dflux_model': lambda x: abc.dflux_models[x],
-    'electrical_model.coil_resistance': lambda x: abc.coil_resistance[x]
-}
+def build_groundtruth_data(which_device, which_sample, save_to_disk=True):
+    """Prepare and return the groundtruth data for an experiment.
 
-translation_dict = {
-    'mechanical_model.damper': 'friction_damping',
-    'mechanical_model.mechanical_spring': 'spring_damping',
-    'coupling_model': 'coupling_factor'
-}
+    Also has the side effect of writing the groundtruth to file.
+    """
 
-# Build the grid to search
-param_grid, val_grid = build_paramater_grid(param_dict, func_dict)
+    mechanical_groundtruth_dict = get_mechanical_groundtruth(
+        which_device,
+        which_sample
+    )
+    electrical_groundtruth_dict = get_electrical_groundtruth(
+        which_device,
+        which_sample
+    )
 
-# Prepare scoring objects + groundtruth
-labeled_video_processor = LabeledVideoProcessor(
-    L=125,
-    mm=10,
-    seconds_per_frame=seconds_per_frame,
-    pixel_scale=pixel_scale)
+    if save_to_disk:
+        export_groundtruth(which_device,
+                           which_sample,
+                           mechanical_groundtruth_dict,
+                           electrical_groundtruth_dict)
 
-voltage_division_ratio = 1/0.342
-adc_processor = AdcProcessor(
-    voltage_division_ratio=voltage_division_ratio,
-    smooth=True)
-
-# Metrics
-mechanical_metrics = {'dtw': dtw_euclid_distance}
-mechanical_v_metrics = {'dtw': dtw_euclid_distance}
-electrical_metrics = {'rms_perc_diff': root_mean_square_percentage_diff,
-                      'dtw': dtw_euclid_distance}
-
-metrics = {
-    'mechanical': mechanical_metrics,
-    'electrical': electrical_metrics
-}
-
-mech_scores = []
-mech_v_scores = []
-elec_scores = []
-
-# Target values
-y_target, y_time_target = labeled_video_processor.fit_transform(
-    samples[which_device][which_sample].video_labels_df,
-    impute_missing_values=True
-)
-### SMOOTH TARGET VALUES  (OPTIONAL?)
-y_target = signal.savgol_filter(y_target, 9, 3)
-
-emf_target, emf_time_target = adc_processor.fit_transform(
-    samples[which_device][which_sample].adc_df
-)
-yv_target = signal.savgol_filter(y_target, 9, 4)
-yv_target = np.gradient(yv_target)/np.gradient(y_time_target)
+    return mechanical_groundtruth_dict, electrical_groundtruth_dict
 
 
 @ray.remote
@@ -171,6 +115,7 @@ def run_cell(base_unified_model,
              parameter_set,
              y_target,
              y_time_target,
+             emf_time_target,
              emf_target,
              metrics):
     """Run a single cell of a gridsearch."""
@@ -221,22 +166,25 @@ def run_cell(base_unified_model,
     return scores, curves
 
 
-jobs = []
-param_log = {}
-
-
 def indexify(array_like, step):
     """Get the indexes to step through an array-like"""
     total_size = len(array_like)
     indexes = np.arange(0, total_size, step)
 
     if indexes[-1] < total_size:
-        indexes = np.append(indexes, total_size -1)
+        indexes = np.append(indexes, total_size)
     return zip(indexes, indexes[1:])
 
 
-def execute_in_batches(param_grid, batch_size=48):
-    """Execute the gridsearch in batches using Ray."""
+def execute_in_batches(base_unified_model,
+                       param_grid,
+                       y_time_target,
+                       y_target,
+                       emf_time_target,
+                       emf_target,
+                       metrics,
+                       batch_size=48):
+    """Execute the grid search in batches."""
 
     start_time = None
     current_number = 0
@@ -267,7 +215,7 @@ def execute_in_batches(param_grid, batch_size=48):
             total=len(param_grid)
         )
         # Then retrieve results
-        results = list(get_ray_results(jobs))  # Stop from getting consumed 
+        results = list(get_ray_results(jobs))  # Stop from getting consumed
         scores = np.append(scores, np.array([x[0] for x in results]))
         curves = np.append(curves, np.array([x[1] for x in results]))
 
@@ -291,8 +239,8 @@ def wait_for_completion(jobs, start_time=None, current_number=0, total=None):
         num_completed = len(ready) + current_number
 
         current_time = time.time()
-        minutes = int((current_time - start_time)/60)
-        seconds = int((current_time - start_time)%60)
+        minutes = int((current_time - start_time) / 60)
+        seconds = int((current_time - start_time) % 60)
         print(f'Progress --- {num_completed} out of {total} --- {minutes}m{seconds}s')
 
         if num_ready == len(jobs):
@@ -350,16 +298,171 @@ def scores_to_dataframe(scores_dict,
     return pd.DataFrame(accumulated_scores)
 
 
+# ═════════════════════════════════
+# Data preparation
+# ═════════════════════════════════
+base_unified_model = UnifiedModel.load_from_disk('../my_saved_model/')
+
+base_groundtruth_path = './data/2019-05-23/'
+
+pixel_scales = {'A': 0.18451, 'B': 0.154508, 'C': 0.154508} # 'B': 0.148148}
+seconds_per_frames = {'A': 1/80, 'B': 1/60, 'C': 1/60}
+samples = {}
+samples['A'] = collect_samples(base_path=base_groundtruth_path,
+                               acc_pattern='A/*acc*.csv',
+                               adc_pattern='A/*adc*.csv',
+                               labeled_video_pattern='A/*labels*.csv')
+samples['B'] = collect_samples(base_path=base_groundtruth_path,
+                               acc_pattern='B/*acc*.csv',
+                               adc_pattern='B/*adc*.csv',
+                               labeled_video_pattern='B/*labels*.csv')
+samples['C'] = collect_samples(base_path=base_groundtruth_path,
+                               acc_pattern='C/*acc*.csv',
+                               adc_pattern='C/*adc*.csv',
+                               labeled_video_pattern='B/*labels*.csv')
+accelerometer_inputs = {}
+accelerometer_inputs['A'] = [
+    AccelerometerInput(
+        raw_accelerometer_input=sample.acc_df,
+        accel_column='z_G',
+        time_column='time(ms)',
+        accel_unit='g',
+        time_unit='ms',
+        smooth=True,
+        interpolate=True)
+    for sample
+    in samples['A']
+]
+
+accelerometer_inputs['B'] = [
+    AccelerometerInput(
+        raw_accelerometer_input=sample.acc_df,
+        accel_column='z_G',
+        time_column='time(ms)',
+        accel_unit='g',
+        time_unit='ms',
+        smooth=True,
+        interpolate=True)
+    for sample
+    in samples['B']
+]
+
+accelerometer_inputs['C'] = [
+    AccelerometerInput(
+        raw_accelerometer_input=sample.acc_df,
+        accel_column='z_G',
+        time_column='time(ms)',
+        accel_unit='g',
+        time_unit='ms',
+        smooth=True,
+        interpolate=True)
+    for sample
+    in samples['C']
+]
+
+# ═════════════════════════════════
+# Experiment Details
+# ═════════════════════════════════
+which_device = 'A'
+which_sample = 2
+
+pixel_scale = pixel_scales[which_device]
+seconds_per_frame = seconds_per_frames[which_device]
+
+accelerometer_input = accelerometer_inputs[which_device]
+input_ = [which_sample]
+
+# ═════════════════════════════════
+# Prepare Ground truth
+# ═════════════════════════════════
+y_groundtruth, emf_groundtruth = build_groundtruth_data(
+    which_device,
+    which_sample
+)
+y_time_target = y_groundtruth['time']
+y_target = y_groundtruth['values']
+emf_time_target = emf_groundtruth['time']
+emf_target = emf_groundtruth['values']
+
+# ═════════════════════════════════
+# Gridsearch parameters
+# ═════════════════════════════════
+damping_coefficients = np.linspace(0.02, 0.05, 20)
+mech_spring_coefficients = np.linspace(0, 0.5, 5) #[0.0125]  # Found from investigation
+constant_coupling_values = np.linspace(0.1, 3, 10)
+rectification_drop = [0.10]
+
+# Overrides (set parameters for test)
+# damping_coefficients = [0.035]
+# mech_spring_coefficients = [0.0000]
+# constant_coupling_values = [0.5]
+
+param_dict = {
+    'mechanical_model.input_': [which_sample],
+    'mechanical_model.damper': damping_coefficients,
+    'mechanical_model.mechanical_spring': mech_spring_coefficients,
+    'coupling_model': constant_coupling_values,
+    'electrical_model.rectification_drop': rectification_drop,
+    'electrical_model.flux_model': [which_device],
+    'electrical_model.dflux_model': [which_device],
+    'electrical_model.coil_resistance': [which_device]
+}
+
+func_dict = {
+    'mechanical_model.input_': lambda x: accelerometer_input[x],
+    'mechanical_model.damper': DamperConstant,
+    'mechanical_model.mechanical_spring': make_mechanical_spring,
+    'coupling_model': ConstantCoupling,
+    'electrical_model.rectification_drop': lambda x: x,
+    'electrical_model.flux_model': lambda x: abc.flux_models[x],
+    'electrical_model.dflux_model': lambda x: abc.dflux_models[x],
+    'electrical_model.coil_resistance': lambda x: abc.coil_resistance[x]
+}
+
+translation_dict = {
+    'mechanical_model.damper': 'friction_damping',
+    'mechanical_model.mechanical_spring': 'spring_damping',
+    'coupling_model': 'coupling_factor'
+}
+
+# Build the grid to search
+param_grid, val_grid = build_paramater_grid(param_dict, func_dict)
+
+
+# Metrics
+mechanical_metrics = {'dtw': dtw_euclid_distance}
+mechanical_v_metrics = {'dtw': dtw_euclid_distance}
+electrical_metrics = {'rms_perc_diff': root_mean_square_percentage_diff,
+                      'dtw': dtw_euclid_distance}
+
+metrics = {
+    'mechanical': mechanical_metrics,
+    'electrical': electrical_metrics
+}
+
+
 # Execute
 ray.init()
-scores, curves = execute_in_batches(param_grid)
+scores, curves = execute_in_batches(
+    base_unified_model=base_unified_model,
+    param_grid=param_grid,
+    y_time_target=y_time_target,
+    y_target=y_target,
+    emf_time_target=emf_time_target,
+    emf_target=emf_target,
+    metrics=metrics
+)
 ray.shutdown()
 
 df_scores = scores_to_dataframe(scores, val_grid, translation_dict)
-df_curves = curves_to_dataframe(curves, sampling_rate=4)
+df_curves = curves_to_dataframe(curves, sampling_rate=3)
 
-df_scores.to_csv(f'{which_device}_{which_sample}_score.csv')
-df_curves.to_csv(f'{which_device}_{which_sample}_curves.xz')
+df_scores.to_parquet(f'{which_device}_{which_sample}_score.parq',
+                     engine='pyarrow',
+                     compression='brotli')
+df_curves.to_parquet(f'{which_device}_{which_sample}_curves.parq',
+                     engine='pyarrow',
+                     compression='brotli')
 
 
 def plot_comparison(df_scores,
@@ -395,15 +498,15 @@ def plot_comparison(df_scores,
     p.__repr__()
 
 
-plot_comparison(
-    df_scores,
-    df_curves,
-    y_time_target,
-    y_target,
-    'y',
-    'mech_dtw',
-    n=1
-)
+# plot_comparison(
+#     df_scores,
+#     df_curves,
+#     emf_time_target,
+#     emf_target,
+#     'emf',
+#     'elec_dtw',
+#     n=1
+# )
 
 
 # df_scores = df_scores.reset_index()
