1980eb0dbb3ec6be53e068a7c06783695d50e309
Michael Struwig <michael@praelexis.com>
2019-12-18 18:55:59 +0200

    Add empty experiment file

---
+++ experiments/2019-12-18-multiple-coil-prototype.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+from config import abc
+
+from unified_model.unified import UnifiedModel
+from unified_model.coupling import ConstantCoupling
+from unified_model.
