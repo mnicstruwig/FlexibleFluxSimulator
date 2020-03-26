6c6894a8f372cf4cbc8f438d444a502555aa46f5
Michael Struwig <michael@praelexis.com>
2020-03-26 22:07:34 +0200

    Move gridsearch logic to a class

--- experiments/2020-03-13-refactor-gridsearch.py
+++ experiments/2020-03-13-refactor-gridsearch.py
@@ -1,147 +1,18 @@
+from collections import namedtuple
+
 import numpy as np
-import pandas as pd
-import ray
 from scipy.signal import savgol_filter
 
-from itertools import product
-from collections import namedtuple, defaultdict
-import logging
-from copy import copy
-
-from unified_model import UnifiedModel
-from unified_model import MechanicalModel
-from unified_model import ElectricalModel
 from unified_model import CouplingModel
 from unified_model import mechanical_components
 from unified_model import electrical_components
 from unified_model import governing_equations
-from unified_model import pipeline
 from unified_model import evaluate
 from unified_model import metrics
+from unified_model import gridsearch
 
 from unified_model.utils.utils import collect_samples
 from config import abc_config
-
-logging.basicConfig(format='%(asctime)s :: %(levelname)s :: %(message)s', level=logging.INFO)
-
-class AbstractUnifiedModelFactory:
-    def __init__(self,
-                 mechanical_components,
-                 electrical_components,
-                 coupling_models,
-                 governing_equations,
-                 ):
-        self.mechanical_components = mechanical_components
-        self.electrical_components = electrical_components
-        self.coupling_models = coupling_models
-        self.governing_equations = governing_equations
-
-        self.combined = {}
-        self.combined.update(mechanical_components)
-        self.combined.update(electrical_components)
-        self.combined['coupling_model'] = coupling_models
-        self.combined['governing_equations' ] = self.governing_equations
-        self.passed_kwargs = []
-
-    def generate(self):
-        """Generate a UnifiedModelFactory for a combination of all components
-
-        Each yielded UnifiedModelFactory can produce a UnifiedModel that
-        consists of one of all the possible combination of `mechanical`-
-        `electrical`-, and `coupling`-models and governing equations specified
-        in the constructor.
-
-        Yields
-        ------
-        UnifiedModelFactory
-            A unified model factory containing one combination of parameters.
-
-        """
-        param_product = product(*[v for v in self.combined.values()])
-        for pp in param_product:
-            new_kwargs = {k:v for k, v in zip(self.combined.keys(), pp)}
-            self.passed_kwargs.append(new_kwargs)
-            yield UnifiedModelFactory(**new_kwargs)
-
-
-class UnifiedModelFactory:
-    """A factory that produces UnifiedModel object.
-
-    Not designed to be used directly -- use the `AbstractUnifiedModelFactory`
-    instead.
-    """
-
-    def __init__(self,
-                 damper=None,
-                 magnet_assembly=None,
-                 magnetic_spring=None,
-                 mechanical_spring=None,
-                 input_excitation=None,
-                 coil_resistance=None,
-                 rectification_drop=None,
-                 load_model=None,
-                 flux_model=None,
-                 dflux_model=None,
-                 coupling_model=None,
-                 governing_equations=None):
-        self.damper=damper
-        self.magnet_assembly=magnet_assembly
-        self.magnetic_spring = magnetic_spring
-        self.mechanical_spring = mechanical_spring
-        self.input_excitation=input_excitation
-        self.coil_resistance=coil_resistance
-        self.rectification_drop=rectification_drop
-        self.load_model=load_model
-        self.flux_model=flux_model
-        self.dflux_model=dflux_model
-        self.coupling_model=coupling_model
-        self.governing_equations=governing_equations
-
-    def get_args(self, param_filter=None):
-        if param_filter is None:
-            return self.__dict__
-        return get_params_of_interest(self.__dict__, param_filter)
-
-    def make(self):
-        mechanical_model = (
-            MechanicalModel()
-            .set_input(self.input_excitation)
-            .set_damper(self.damper)
-            .set_magnet_assembly(self.magnet_assembly)
-            .set_mechanical_spring(self.mechanical_spring)
-            .set_magnetic_spring(self.magnetic_spring)
-        )
-        electrical_model = (
-            ElectricalModel()
-            .set_coil_resistance(self.coil_resistance)
-            .set_rectification_drop(self.rectification_drop)
-            .set_load_model(self.load_model)
-            .set_flux_model(self.flux_model, self.dflux_model)
-        )
-        unified_model = (
-            UnifiedModel()
-            .set_mechanical_model(mechanical_model)
-            .set_electrical_model(electrical_model)
-            .set_coupling_model(self.coupling_model)
-            .set_governing_equations(self.governing_equations)
-            .set_post_processing_pipeline(pipeline.clip_x2, name='clip x2')
-        )
-        return unified_model
-
-    def get_parameters_as_str(self, parameters_to_get):
-
-        def _walk_attrs(obj, search_str):
-            split_ = search_str.split('.')
-            value = obj
-            for s in split_:
-                value = value.__dict__[s]
-            return value
-
-        parameter_str = ''
-        for param in parameters_to_get:
-            attr_value = _walk_attrs(self, param)
-            parameter_str += f'{param}: {attr_value}\n'
-        return parameter_str
 
 
 class ConstantDamperFactory:
@@ -169,13 +40,14 @@ class CouplingModelFactory:
         self.coupling_factor_list = coupling_factor_list
 
     def make(self):
-        return [CouplingModel().set_coupling_constant(c) for c in self.coupling_factor_list]
+        return [CouplingModel().set_coupling_constant(c)
+                for c in self.coupling_factor_list]
 
 
 class AccelerometerInputsFactory:
     def __init__(self, sample_list, acc_input_kwargs=None):
         self.sample_list = sample_list
-        self.acc_input_kwargs = {} if acc_input_kwargs is None else acc_input_kwargs
+        self.acc_input_kwargs = {} if acc_input_kwargs is None else acc_input_kwargs  # noqa
         self._set_defaults()
 
     def _set_defaults(self):
@@ -191,12 +63,12 @@ class AccelerometerInputsFactory:
         for sample in self.sample_list:
             acc_input = mechanical_components.AccelerometerInput(
                 raw_accelerometer_input=sample.acc_df,
-                accel_column=self.acc_input_kwargs.setdefault('accel_column', 'z_G'),
-                time_column=self.acc_input_kwargs.setdefault('time_column', 'time(ms)'),
+                accel_column=self.acc_input_kwargs.setdefault('accel_column', 'z_G'),  # noqa
+                time_column=self.acc_input_kwargs.setdefault('time_column', 'time(ms)'),  # noqa
                 accel_unit=self.acc_input_kwargs.setdefault('accel_unit', 'g'),
                 time_unit=self.acc_input_kwargs.setdefault('time_unit', 'ms'),
                 smooth=self.acc_input_kwargs.setdefault('smooth', True),
-                interpolate=self.acc_input_kwargs.setdefault('interpolate', True)
+                interpolate=self.acc_input_kwargs.setdefault('interpolate', True)  # noqa
             )
             accelerometer_inputs.append(acc_input)
         return accelerometer_inputs
@@ -210,8 +82,10 @@ class GroundTruthFactory:
 
         self.lvp = evaluate.LabeledVideoProcessor(**lvp_kwargs)
         self.adc = evaluate.AdcProcessor(**adc_kwargs)
-        self.MechGroundtruth = namedtuple('MechanicalGroundtruth', ['y_diff', 'time'])
-        self.ElecGroundtruth = namedtuple('ElectricalGroundtruth', ['emf', 'time'])
+        self.MechGroundtruth = namedtuple('MechanicalGroundtruth',
+                                          ['y_diff', 'time'])
+        self.ElecGroundtruth = namedtuple('ElectricalGroundtruth',
+                                          ['emf', 'time'])
         self.Groundtruth = namedtuple('Groundtruth', ['mech', 'elec'])
 
     def _make_mechanical_groundtruth(self, sample):
@@ -222,7 +96,7 @@ class GroundTruthFactory:
         y_target = savgol_filter(y_target, 9, 3)
 
         return self.MechGroundtruth(y_target,
-                                   y_time_target)
+                                    y_time_target)
 
     def _make_electrical_groundtruth(self, sample):
         emf_target, emf_time_target = self.adc.fit_transform(sample.adc_df)
@@ -242,179 +116,7 @@ class GroundTruthFactory:
         return groundtruths
 
 
-def chunk(array_like, chunk_size):
-    """Chunk up an array-like. Generator"""
-    total_size = len(array_like)
-    indexes = list(range(0, total_size, chunk_size))
-
-    # Make sure we get the final chunk:
-    if indexes[-1] < total_size:
-        indexes.append(total_size)
-
-    for start, stop in zip(indexes, indexes[1:]):
-        yield array_like[start:stop]
-
-@ray.remote
-def run_cell(unified_model_factory, groundtruth, metrics):
-    model = unified_model_factory.make()
-    model.solve(t_start=0,
-                t_end=8,
-                t_max_step=1e-3,
-                y0=[0.0, 0.0, 0.04, 0.0, 0.0])
-
-    mech_score, mech_eval = model.score_mechanical_model(
-        time_target=groundtruth.mech.time,
-        y_target=groundtruth.mech.y_diff,
-        metrics_dict=metrics['mechanical'],
-        prediction_expr='x3-x1',
-        return_evaluator=True
-    )
-
-    elec_score, elec_eval = model.score_electrical_model(
-        time_target=groundtruth.elec.time,
-        emf_target=groundtruth.elec.emf,
-        metrics_dict=metrics['electrical'],
-        prediction_expr='g(t, x5)',
-        return_evaluator=True,
-        clip_threshold=1e-1
-    )
-
-    scores = {'mechanical': mech_score,
-              'electrical': elec_score}
-
-    curves = {'time': mech_eval.time_,
-              'y_diff': mech_eval.y_predict_,
-              'emf': elec_eval.emf_predict_}
-
-    return scores, curves
-
-
-def execute_gridsearch_for_sample(abstract_unified_model_factory,
-                                  groundtruth,
-                                  metrics,
-                                  parameters_to_track,
-                                  batch_size=8):
-
-    # This doesn't make me happy, but I'm out of clean ideas for now If I want
-    # this to work and _also_ preserve order.
-    model_factories = list(abstract_unified_model_factory.generate())
-    total_completed = 0
-    total_tasks = len(model_factories)
-
-    grid_scores = []
-    grid_curves = []
-    grid_params = []
-    for model_factory_batch in chunk(model_factories, batch_size):
-        task_queue = []
-        for model_factory in model_factory_batch:
-            grid_params.append(model_factory.get_args(parameters_to_track))
-            task_id = run_cell.remote(model_factory, groundtruth, metrics)
-            task_queue.append(task_id)
-
-        ready = []
-        while len(ready) < len(task_queue):
-            ready, remaining = ray.wait(task_queue, num_returns=len(task_queue), timeout=5.)
-            logging.info(f'Progress: {len(ready)+total_completed}/{total_tasks}')
-
-        # Once all tasks are completed...
-        total_completed += len(ready)# ... increment the total completed counter ...
-        results = [ray.get(task_id) for task_id in task_queue]  # ... and fetch results
-
-        # Parse the results
-        for result in results:
-            grid_scores.append(copy(result[0]))
-            grid_curves.append(copy(result[1]))
-
-        del results  # Remove reference so Ray can free memory as needed
-    return grid_scores, grid_curves, grid_params
-
-
-def get_nested_param(obj, path):
-    split_ = path.split('.')
-    temp = obj[split_[0]]
-    for s in split_[1:]:
-        if isinstance(temp, dict):  # If we have a dict...
-            temp = temp[s]
-        else:  # If we have an object
-            temp = temp.__dict__[s]
-    return temp
-
-def get_params_of_interest(param_dict, params_of_interest):
-    if not isinstance(param_dict, dict):
-        raise TypeError('param_dict is not a dict')
-    if not isinstance(params_of_interest, list):
-        raise TypeError('params_of_interest is not a list')
-
-    result = {}
-    for param in params_of_interest:
-        result[param] = get_nested_param(param_dict, param)
-    return result
-
-def parse_score_dict(score_dict):
-    parsed_score = {}
-    for category, score_named_tuple in score_dict.items():
-        for metric, value in score_named_tuple._asdict().items():
-            metric_name = category[:4] + '_' + metric
-            parsed_score[metric_name] = value
-    return parsed_score
-
-def scores_to_dataframe(grid_scores):
-    parsed_scores = [parse_score_dict(score_dict)
-                     for score_dict
-                     in grid_scores]
-
-    result = defaultdict(list)
-    for i, score_dict in enumerate(parsed_scores):
-        for key, val in score_dict.items():
-            result[key].append(val)
-
-        result['param_set_id'].append(i)
-    return pd.DataFrame(result)
-
-def curves_to_dataframe(grid_curves, sample_rate):
-
-    result = defaultdict(lambda: np.empty(0))
-
-    for i, curve_dict in enumerate(grid_curves):
-        for key, values in curve_dict.items():
-            subsampled_values = values[::sample_rate]
-            result[key] = np.concatenate([result[key], subsampled_values])
-            values_length = len(subsampled_values)
-
-        param_set_id = np.full(values_length, i)
-        result['param_set_id'] = np.concatenate([result['param_set_id'], param_set_id])
-
-    return pd.DataFrame(result)
-
-
-def param_dict_list_to_dataframe(param_dict_list):
-    result = defaultdict(list)
-    for i, param_dict in enumerate(param_dict_list):
-        for key, value in param_dict.items():
-            result[key].append(value)
-        result['param_set_id'].append(i)
-
-    return pd.DataFrame(result)
-
-
-def process_results(grid_results, curve_subsample_rate=3):
-    grid_scores, grid_curves, grid_params = grid_results
-
-    # Some basic validation
-    assert(len(grid_scores) == len(grid_curves))
-    assert(len(grid_scores) == len(grid_params))
-
-    df_params = param_dict_list_to_dataframe(grid_params)
-    df_scores = scores_to_dataframe(grid_scores)
-    df_curves = curves_to_dataframe(grid_curves, sample_rate=curve_subsample_rate)
-
-    # Merge dataframes
-    result = df_scores.merge(df_params, on='param_set_id')
-    result = result.merge(df_curves, on='param_set_id')
-
-    return result
-
-
+# Groundtruth
 base_groundtruth_path = './data/2019-05-23_C/'
 samples = {}
 samples['A'] = collect_samples(base_path=base_groundtruth_path,
@@ -430,6 +132,7 @@ samples['C'] = collect_samples(base_path=base_groundtruth_path,
                                adc_pattern='C/*adc*.csv',
                                video_label_pattern='B/*labels*.csv')
 
+# Set components
 input_excitation_factories = {k: AccelerometerInputsFactory(samples[k])
                               for k in ['A', 'B', 'C']}
 
@@ -450,9 +153,8 @@ mech_components = {
     'input_excitation': input_excitation_factories['A'].make()[:1],
     'magnetic_spring': [magnetic_spring],
     'magnet_assembly': [magnet_assembly],
-    'damper': ConstantDamperFactory(np.linspace(0.01, 0.07, 10)).make(),  # np.linspace(0.01, 0.07, 5)
-    'mechanical_spring':  MechanicalSpringFactory(110/1000,
-                                                  [0]).make(),
+    'damper': ConstantDamperFactory(np.linspace(0.01, 0.07, 10)).make(),
+    'mechanical_spring':  MechanicalSpringFactory(110/1000, [0]).make()
 }
 
 elec_components = {
@@ -468,13 +170,14 @@ governing_equations = [governing_equations.unified_ode]
 
 coupling_models = CouplingModelFactory([0]).make()
 
-abstract_model_factory = AbstractUnifiedModelFactory(
+abstract_model_factory = gridsearch.AbstractUnifiedModelFactory(
     mech_components,
     elec_components,
     coupling_models,
     governing_equations
 )
 
+# Set groundtruth
 groundtruth_factory = GroundTruthFactory(
     samples['A'][:5],
     lvp_kwargs=dict(L=125,
@@ -497,28 +200,20 @@ electrical_metrics = {
 metrics = {'mechanical': mechanical_metrics,
            'electrical': electrical_metrics}
 
-# Get groundtruth and score
+
 groundtruth = groundtruth_factory.make()[0]
 
-# Initialize Ray
-ray.init(
-    memory=8 * 1024 * 1024 * 1024,
-    object_store_memory=4 * 1024 * 1024 * 1024,
-    ignore_reinit_error=True
-)
-
+# Parameters we want to track
 parameters_to_track = [
     'damper.damping_coefficient',
     'coupling_model.coupling_constant',
     'mechanical_spring.damping_coefficient'
 ]
 
-grid_results = execute_gridsearch_for_sample(
-    abstract_model_factory,
-    groundtruth,
-    metrics,
-    parameters_to_track
-)
+# Run the gridsearch
+grid_executor = gridsearch.GridsearchBatchExecutor(abstract_model_factory,
+                                                   groundtruth,
+                                                   metrics,
+                                                   parameters_to_track)
 
-result = process_results(grid_results, 3)
-
+grid_executor.run()  # Execute
