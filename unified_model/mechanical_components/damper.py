71128977940517b2fb5da1e8ee4e48b7c30aeaab
Michael Struwig <mnicstruwig@gmail.com>
2021-03-22 10:26:08 +0200

    Add __repr__ to damper

--- unified_model/mechanical_components/damper.py
+++ unified_model/mechanical_components/damper.py
@@ -3,8 +3,10 @@ import numpy as np
 from unified_model.utils.utils import pretty_str
 from unified_model.mechanical_components.magnet_assembly import MagnetAssembly
 
+
 def _sigmoid(x, x0=0):
     return 1 / (1 + np.exp(-(5*x - x0)))
+
 
 class QuasiKarnoppDamper:
     """A damper that is based on a modified Karnopp friction model."""
@@ -13,15 +15,32 @@ class QuasiKarnoppDamper:
                  motional_damping_coefficient: float,
                  magnet_assembly: MagnetAssembly,
                  tube_inner_radius_mm: float) -> None:
-        """Constructor"""
-        # TODO: Documentation
-        # TODO: __repr__ and __str__
+        """Constructor
+
+        Parameters
+        ----------
+        coulomb_damping_coefficient : float
+            The damping coefficient that controls the amount of viscous
+            friction. Typical values fall in the 1.0 - 6.0 range.
+        motional_damping_coefficient : float
+            The damping coefficient that controls the amount of friction due to
+            the shape of the magnet assembly. Typical values fall in the
+            0.0-0.005 range.
+        magnet_assembly : MagnetAssembly
+            The MagnetAssembly that is being used.
+        tube_inner_radius_mm : float
+            The inner radius of the microgenerator tube in mm.
+
+        """
         self.cdc = coulomb_damping_coefficient
         self.mdc = motional_damping_coefficient
         self.magnet_assembly_length = magnet_assembly.get_length()
         self.magnet_assembly_mass = magnet_assembly.get_mass()
         self.r_t = tube_inner_radius_mm
         self.angle_friction_factor = 2 * self.r_t / self.magnet_assembly_length
+
+    def __repr__(self):
+        return f'QuasiKarnoppDamper(\n  coulomb_damping_coefficient={self.cdc},\n  motional_damping_coefficient={self.mdc},\n  tube_inner_radius_mm={self.r_t}\n)'  # noqa
 
     def get_force(self, velocity, velocity_threshold=0.01):
         """Get the force exerted by the damper."""
